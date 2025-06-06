0f20644a43357195a6c05a7fdfb8b4ee5d5c7354
Jovalentine <166289929+Jovalentine@users.noreply.github.com>
2025-06-06 18:48:19 +0530

    Add files via upload

---
+++ auth.py
@@ -0,0 +1,19 @@
+import firebase_admin
+from firebase_admin import credentials, auth
+
+# Correct path to your firebase key
+cred = credentials.Certificate(r'G:\SmartDoc Summarize\Assets\ai-document-summerize-firebase-adminsdk-fbsvc-4db17133d2.json')
+firebase_admin.initialize_app(cred)
+
+# Function to register user
+def register_user(email, password, username):
+    try:
+        user = auth.create_user(
+            email=email,
+            password=password,
+            display_name=username
+        )
+        return user.uid
+    except Exception as e:
+        print('Error registering user:', e)
+        return None
