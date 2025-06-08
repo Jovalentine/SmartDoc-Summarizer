b31f0f764b3ec3a960b02fe649cb447723c56816
Jovalentine <166289929+Jovalentine@users.noreply.github.com>
2025-06-08 13:51:19 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,9 +1,10 @@
 import firebase_admin
 from firebase_admin import credentials, auth
 
-# Correct path to your firebase key
-cred = credentials.Certificate(dict(st.secrets["firebase"]))
-firebase_admin.initialize_app(cred)
+# Initialize Firebase only once
+if not firebase_admin._apps:
+    cred = credentials.Certificate(dict(st.secrets["firebase"]))
+    firebase_admin.initialize_app(cred)
 
 # Function to register user
 def register_user(email, password, username):
